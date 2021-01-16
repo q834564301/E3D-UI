ec257344c3197435c3c85d05fe3c79c5afbab7f3
Holo <holofox@qq.com>
2021-01-16 17:08:50 +0800

    '项目初始化'

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import './assets/css/common.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
+//  页面标题改变 -> meta.title
+router.beforeEach((to, from, next) => {
+  to.meta && (document.title = to.meta.title)
+  next()
+})
 
 new Vue({
   router,
---
+++ src/views/FileList/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    这就是我现在电脑的页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileList'
+}
+</script>
+
+<style lang="stylus" scoped></style>
